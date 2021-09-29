7569104ce18644caad2d528b5dcb37db0d0a0756
alkomaJunior <alkomajunior@outlook.com>
2021-09-29 20:07:14 +0200

    Try to fix deploy bug

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -49,7 +49,7 @@ class CommentCrudController extends AbstractCrudController
         yield TextField::new('photoFilename')
             ->onlyOnIndex()
         ;
-        yield DateTimeField::new('createdAt')->setTimezone('UTC');
+        yield DateTimeField::new('createdAt')->setTimezone('UTC+2');
     }
 
 }
