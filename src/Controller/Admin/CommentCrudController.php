869942f74d42c35b91f4c25c42373fd917b0d6e3
alkomaJunior <alkomajunior@outlook.com>
2021-09-29 19:27:46 +0200

    Try to fix deploy bug

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -49,17 +49,6 @@ class CommentCrudController extends AbstractCrudController
         yield TextField::new('photoFilename')
             ->onlyOnIndex()
         ;
-        $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions([
-            'html5' => true,
-            'years' => range(date('Y'), date('Y') + 5),
-            'widget' => 'single_text',
-        ]);
-        if (Crud::PAGE_EDIT === $pageName) {
-            yield $createdAt->setFormTypeOption('disabled', true);
-        }
-        else {
-            yield $createdAt;
-        }
     }
 
 }
